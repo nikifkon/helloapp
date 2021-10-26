efc3942222ff7e5dbad30c8bb9fffae2c60b02c5
white <white@example.com>
2021-10-26 13:16:09 +0500

    Add GreetWhite

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace helloapp_black
 {
     class Program
     {
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
